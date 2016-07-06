eb9abf86e1eb21fad51713c6278e0e6fac51dd02
dicristomanuel <manuel.dicristo@raise.com>
2016-07-06 14:46:11 -0500

    [index.js] add babel-polyfill

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+require('babel-polyfill');
+
 export { default as zip_lookup } from './finders/zip_lookup';
 export { default as gps_lookup } from './finders/gps_lookup';
 export { default as findByState } from './finders/findByState';
